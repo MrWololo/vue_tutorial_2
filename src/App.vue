c112f3fefd42ff3b6f3eee7d702d0431d8882faa
MrWololo <martin_pedrosa@hotmail.com>
2021-03-02 19:14:56 -0300

    commit 2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <br>
+  <!-- <input type="text" ref="name" />
+  <button @click="handleClick">Click me</button> -->
+  <div v-if="showModal">
+    <Modal
+      :header="modalHeader"
+      :text="modalText"
+      theme="sale"
+      :showModal="showModal"
+      @close="toggleModal"
+    />
+  </div>
+  <button @click="toggleModal">Switch Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "My First Vue App",
+      modalHeader: "Bruh",
+      modalText: "jejox",
+      showModal: false,
+    };
+  },
+  methods: {
+    // handleClick() {
+    //   console.log(this.$refs.name);
+    //   this.$refs.name.classList.add("active");
+    //   this.$refs.name.focus();
+    // },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +52,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
